602a2ce38dfce062e65a51988c450357123c90c6
Andrew Tolochkevych <andriy410tolochkevitch@gmail.com>
2022-04-27 02:04:26 +0300

    login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,39 @@
 import {FC} from 'react';
-
+import {
+    Formik,
+    FormikHelpers,
+    FormikProps,
+    Form,
+    Field,
+    FieldProps,
+  } from 'formik';
+interface LoginFormValue{
+    login: string;
+    email: string;
+}
 export const Login:FC = () => {
+    const getDataFromForm = (data:LoginFormValue) => {
+        console.log(data)
+    }
     return (
         <div>
-            
+            <Formik initialValues={{
+                login: '',
+                email: ''
+            }}
+            onSubmit={(
+                values: LoginFormValue,
+                {setSubmitting}: FormikHelpers<LoginFormValue>
+            )=>{
+                getDataFromForm(values)
+            }}
+            ><Form>
+                <Field name="login"/>
+                <Field name="email" type="email"/>
+                <button type='submit'>Submit</button>
+            </Form>
+
+            </Formik>
         </div>
     );
 };
